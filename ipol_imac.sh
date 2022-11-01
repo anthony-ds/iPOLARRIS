f754eeffe3a951139bbef8f33f3aab0b2f8df499
Anthony Di Stefano <adistefa@eoas.ubc.ca>
2022-10-31 18:58:49 -0700

    NEW! Multiple start/end times to aggregate over different periods of storm cycle

--- ipol_imac.sh
+++ ipol_imac.sh
@@ -8,12 +8,15 @@ realpath() {
 
 ipoldir="$(realpath )"
 
-starg=$1
-enarg=$2
+sts=$1
+ens=$2
 raddir="$(realpath $3)"
 tempdir="$(realpath $4)"
 doppdir=$5
 declare -a simdirs=(${@:6})
+
+IFS=',' read -r -a starg <<< "$sts"
+IFS=',' read -r -a enarg <<< "$ens"
 
 ptype=mp4
 
@@ -22,16 +25,21 @@ mkdir -p $raddir $tempdir
 declare -a mpopts=( "mp06" "mp08" "mp10" "mp16" "mp51" )
 declare -a mpnames=( "wsm6" "thom" "morr" "wdm6" "p3" )
 
-st=$(echo $starg | sed 's/[^0-9]*//g')
-stdate=${st:0:8}
-sttime=${st:8:4}
-en=$(echo $enarg | sed 's/[^0-9]*//g')
-endate=${en:0:8}
-entime=${en:8:4}
-stt=${stdate}_${sttime}
-edt=${endate}_${entime}
+declare -a stt=()
+declare -a edt=()
 
-configdir=$ipoldir/configtxt/${stt}_${edt}
+for ((ii=0;ii<${#starg[@]};ii++)); do
+    st=$(echo ${starg[ii]} | sed 's/[^0-9]*//g')
+    stdate=${st:0:8}
+    sttime=${st:8:4}
+    en=$(echo ${enarg[ii]} | sed 's/[^0-9]*//g')
+    endate=${en:0:8}
+    entime=${en:8:4}
+    stt+=(${stdate}_${sttime})
+    edt+=(${endate}_${entime})
+done
+
+configdir=$ipoldir/configtxt/${stt[0]}_${edt[${#edt[@]}-1]}
 mkdir -p $configdir
 
 echo Determining data agency and type...
@@ -54,18 +62,18 @@ if [[ "$(ls $raddir/* | head -n 1 | xargs basename)" == "wrfout"* ]]; then
     for ((ii=0;ii<${#mpopts[@]};ii++)); do
        [[ "${mpopts[ii]}" = "$mp" ]] && break
     done
-    inputfile=input_${mpnames[ii]}_${stt}_${edt}.txt
-    configfile=config_${mpnames[ii]}_${stt}_${edt}.txt
+    inputfile=input_${mpnames[ii]}_${stt[0]}_${edt[${#edt[@]}-1]}.txt
+    configfile=config_${mpnames[ii]}_${stt[0]}_${edt[${#edt[@]}-1]}.txt
     echo $data
 else
     if [ -z $simdirs ]; then
         data='obs'
-        inputfile=input_${data}_${stt}_${edt}.txt
-        configfile=config_${data}_${stt}_${edt}.txt
+        inputfile=input_${data}_${stt[0]}_${edt[${#edt[@]}-1]}.txt
+        configfile=config_${data}_${stt[0]}_${edt[${#edt[@]}-1]}.txt
         echo $data
     else
-        inputfile=input_obs_${stt}_${edt}.txt
-        configfile=config_obs_${stt}_${edt}.txt
+        inputfile=input_obs_${stt[0]}_${edt[${#edt[@]}-1]}.txt
+        configfile=config_obs_${stt[0]}_${edt[${#edt[@]}-1]}.txt
         echo "obs vs wrf"
     fi
 fi
@@ -80,69 +88,104 @@ maxelev=$(cat $path2specs | grep "max_elev_angle =" | cut -d '=' -f2 | cut -d ';
 band=$(cat $path2specs | grep "frequency_band =" | cut -d '=' -f2 | cut -d ';' -f1 | xargs)
 
 echo
-echo Selecting radar files for analysis in range $stt to $edt...
+echo Selecting radar files for analysis in range ${stt[0]} to ${edt[${#edt[@]}-1]}...
 echo
 sleep 3
 
-tfile=input_${stt}_${edt}.txt
+tfile=input_${stt[0]}_${edt[${#edt[@]}-1]}.txt
+rm -f $configdir/$tfile
 
-for filepath in $(ls $raddir/* | sort); do
-    file=$(basename $filepath)
+for ((ii=0;ii<${#starg[@]};ii++)); do
+ 
     if [[ "$(ls $raddir/* | head -n 1 | xargs basename)" == "wrfout"* ]]; then
-        filedt=$(echo $file | cut -d '_' -f4 | tr -d '-')$(echo $file | cut -d '_' -f5 | cut -d '.' -f1 | tr -d ':')
+        first=${stt[ii]:0:4}-${stt[ii]:4:2}-${stt[ii]:6:2}_${stt[ii]:9:2}
+        last=${edt[ii]:0:4}-${edt[ii]:4:2}-${edt[ii]:6:2}_${edt[ii]:9:2}
+        fmt=%Y-%m-%d_%H
+        len=13
     else
-        filedt=$(echo $file | cut -d '_' -f2)$(echo $file | cut -d '_' -f3)
+        first="${stt[ii]:0:8}_${stt[ii]:9:2}"
+        last="${edt[ii]:0:8}_${edt[ii]:9:2}"
+        fmt=%Y%m%d_%H
+        len=11
     fi
-    if [ "$filedt" -ge "$(echo $stt | tr -d '_')00" ] && [ "$filedt" -lt "$(echo $edt | tr -d '_')00" ]; then 
-        echo $filepath >> $configdir/$tfile
-        echo $(basename $filepath)
-    fi
-    if [ "$filedt" -ge "$(echo $edt | tr -d '_')00" ]; then
-        break
-    fi
+    dats=$(echo ${first:0:$len})
+    while [[ $(echo $first | tr -d '-' | tr -d '_' | tr -d ':') -lt $(echo $last | tr -d '-' | tr -d '_' | tr -d ':') ]]; do
+        first=$(date -j -v +1H -f $fmt $first +$fmt)
+        dats=$dats"\|"${first:0:$len}
+    done
+
+    for filepath in $(ls $raddir/* | grep "$dats" | sort); do
+        file=$(basename $filepath)
+        if [[ "$(ls $raddir/* | head -n 1 | xargs basename)" == "wrfout"* ]]; then
+            filedt=$(echo $file | cut -d '_' -f4 | tr -d '-')$(echo $file | cut -d '_' -f5 | cut -d '.' -f1 | tr -d ':')
+        else
+            filedt=$(echo $file | cut -d '_' -f2)$(echo $file | cut -d '_' -f3)
+        fi
+        if [ "$filedt" -ge "$(echo ${stt[ii]} | tr -d '_')00" ] && [ "$filedt" -lt "$(echo ${edt[ii]} | tr -d '_')00" ]; then 
+            echo $filepath >> $configdir/$tfile
+            echo $(basename $filepath)
+        fi
+        if [ "$filedt" -ge "$(echo ${edt[ii]} | tr -d '_')00" ]; then
+            break
+        fi
+    done
+
 done
 
 mv $configdir/$tfile $configdir/$inputfile
 
-tfile=tmp_${stt}_${edt}.txt
+echo
+echo Selecting temperature files for analysis in range ${stt[0]} to ${edt[${#edt[@]}-1]}...
+echo
+sleep 3 
+
+tfile=tmp_${stt[0]}_${edt[${#edt[@]}-1]}.txt
+rm -f $configdir/$tfile
 
 if [[ "$(ls $tempdir/* | sort | head -n 1 | xargs basename)" == "wrfout"* ]]; then
-
-    echo
-    echo Selecting temperature files for analysis in range $stt to $edt...
-    echo
-    sleep 3 
-
-    for filepath in $(ls $tempdir/* | sort); do
-         
-        file=$(basename $filepath)
-        filedt=$(echo $file | cut -d '_' -f4 | tr -d '-')$(echo $file | cut -d '_' -f5 | cut -d '.' -f1 | tr -d ':')
-        if [ "$filedt" -ge "$(echo $stt | tr -d '_')00" ] && [ "$filedt" -le "$(echo $edt | tr -d '_')00" ]; then 
-            echo $filepath >> $configdir/$tfile
-            echo $(basename $filepath)
-        fi
-        if [ "$filedt" -gt "$(echo $edt | tr -d '_')00" ]; then
-            break
-        fi
-
-    done
-
-    mp=$(head -n 1 $configdir/$tfile | xargs basename | cut -d '_' -f2)
+    
+    mp=$(ls $tempdir/* | sort | head -n 1 | xargs basename | cut -d '_' -f2)
     for ((ii=0;ii<${#mpopts[@]};ii++)); do
        [[ "${mpopts[ii]}" = "$mp" ]] && break
     done
     mpname=$(echo "${mpnames[ii]}")
     tempsrc=$mpname
-    tempfile=temp_${tempsrc}_${stt}_${edt}.txt
+    tempfile=temp_${tempsrc}_${stt[0]}_${edt[${#edt[@]}-1]}.txt
     snd_on='False'
     wrft_on='True'
 
+    for ((ii=0;ii<${#starg[@]};ii++)); do
+        
+        first=${stt[ii]:0:4}-${stt[ii]:4:2}-${stt[ii]:6:2}_${stt[ii]:9:2}
+        last=${edt[ii]:0:4}-${edt[ii]:4:2}-${edt[ii]:6:2}_${edt[ii]:9:2}
+        fmt=%Y-%m-%d_%H
+        len=13
+        dats=$(echo ${first:0:$len})
+        while [[ $(echo $first | tr -d '-' | tr -d '_' | tr -d ':') -lt $(echo $last | tr -d '-' | tr -d '_' | tr -d ':') ]]; do
+            first=$(date -j -v +1H -f $fmt $first +$fmt)
+            dats=$dats"\|"${first:0:$len}
+        done
+
+        for filepath in $(ls $tempdir/* | grep "$dats" | sort); do
+            file=$(basename $filepath)
+            filedt=$(echo $file | cut -d '_' -f4 | tr -d '-')$(echo $file | cut -d '_' -f5 | cut -d '.' -f1 | tr -d ':')
+            if [ "$filedt" -ge "$(echo ${stt[ii]} | tr -d '_')00" ] && [ "$filedt" -le "$(echo ${edt[ii]} | tr -d '_')00" ]; then 
+                echo $filepath >> $configdir/$tfile
+                echo $(basename $filepath)
+            fi
+            if [ "$filedt" -gt "$(echo ${edt[ii]} | tr -d '_')00" ]; then
+                break
+            fi
+        done
+
+    done
+
 else
 
-    echo
-    echo Selecting sounding files at closest times to range $stt to $edt...
-    echo
-    sleep 3
+    tempsrc='uwyo-'$(basename $tempdir | tr '[:upper:]' '[:lower:]')
+    tempfile=temp_${tempsrc}_${stt[0]}_${edt[${#edt[@]}-1]}.txt
+    snd_on='True'
+    wrft_on='False'
 
     declare -a beffiles=()
     declare -a infiles=()
@@ -153,11 +196,11 @@ else
         file=$(basename $filepath)
         filedt=$(echo $file | cut -d '_' -f2 | tr -d '-')$(echo $file | cut -d '_' -f3 | cut -d '.' -f1 | tr -d ':')
        
-        if [ "$filedt" -lt "$(echo $stt | tr -d '_')00" ]; then
+        if [ "$filedt" -lt "$(echo ${stt[ii]} | tr -d '_')00" ]; then
             beffiles+=($filepath)
-        elif [ "$filedt" -ge "$(echo $stt | tr -d '_')00" ] && [ "$filedt" -le "$(echo $edt | tr -d '_')00" ]; then
+        elif [ "$filedt" -ge "$(echo ${stt[ii]} | tr -d '_')00" ] && [ "$filedt" -le "$(echo ${edt[ii]} | tr -d '_')00" ]; then
             infiles+=($filepath)
-        elif [ "$filedt" -gt "$(echo $edt | tr -d '_')00" ]; then 
+        elif [ "$filedt" -gt "$(echo ${edt[ii]} | tr -d '_')00" ]; then 
             aftfiles+=($filepath)
         fi
         
@@ -172,11 +215,6 @@ else
     echo $(basename ${aftfiles[0]})
     echo ${aftfiles[0]} >> $configdir/$tfile
     
-    tempsrc='uwyo-'$(basename $tempdir | tr '[:upper:]' '[:lower:]')
-    tempfile=temp_${tempsrc}_${stt}_${edt}.txt
-    snd_on='True'
-    wrft_on='False'
-
 fi
 
 mv $configdir/$tfile $configdir/$tempfile
@@ -189,25 +227,46 @@ if [ ! -z $doppdir ]; then
     sleep 3
 
     dd_on='True'
-    doppfile=dopp_${data}_${stt}_${edt}.txt
+    doppfile=dopp_${data}_${stt[0]}_${edt[${#edt[@]}-1]}.txt
     
     tfile=dopp.txt
+    rm -f $configdir/$tfile
 
-    for filepath in $(ls $doppdir/* | sort); do
-        file=$(basename $filepath) 
-        if [[ "$(ls $raddir/* | head -n 1 | xargs basename)" == "wrfout"* ]]; then
-            filedt=$(echo $file | cut -d '_' -f4 | tr -d '-')$(echo $file | cut -d '_' -f5 | cut -d '.' -f1 | tr -d ':')
+    for ((ii=0;ii<${#starg[@]};ii++)); do
+
+        if [[ "$(ls $doppdir/* | head -n 1 | xargs basename)" == "wrfout"* ]]; then
+            first=${stt[ii]:0:4}-${stt[ii]:4:2}-${stt[ii]:6:2}_${stt[ii]:9:2}
+            last=${edt[ii]:0:4}-${edt[ii]:4:2}-${edt[ii]:6:2}_${edt[ii]:9:2}
+            fmt=%Y-%m-%d_%H
+            len=13
         else
-            #filedt=$(echo $file | cut -d '_' -f2)$(echo $file | cut -d '_' -f3)
-            filedt=$(echo $file | cut -d '_' -f3)$(echo $file | cut -d '_' -f4)
+            first="${stt[ii]:0:8}_${stt[ii]:9:2}"
+            last="${edt[ii]:0:8}_${edt[ii]:9:2}"
+            fmt=%Y%m%d_%H
+            len=11
         fi
-        if [ "$filedt" -ge "$(echo $stt | tr -d '_')00" ] && [ "$filedt" -lt "$(echo $edt | tr -d '_')00" ]; then 
-            echo $filepath >> $configdir/$tfile
-            echo $(basename $filepath)
-        fi
-        if [ "$filedt" -ge "$(echo $edt | tr -d '_')00" ]; then
-            break
-        fi
+        dats=$(echo ${first:0:$len})
+        while [[ $(echo $first | tr -d '-' | tr -d '_' | tr -d ':') -lt $(echo $last | tr -d '-' | tr -d '_' | tr -d ':') ]]; do
+            first=$(date -j -v +1H -f $fmt $first +$fmt)
+            dats=$dats"\|"${first:0:$len}
+        done
+
+        for filepath in $(ls $doppdir/* | grep "$dats" | sort); do
+            file=$(basename $filepath)  
+            if [[ "$(ls $doppdir/* | head -n 1 | xargs basename)" == "wrfout"* ]]; then
+                filedt=$(echo $file | cut -d '_' -f4 | tr -d '-')$(echo $file | cut -d '_' -f5 | cut -d '.' -f1 | tr -d ':')
+            else
+                filedt=$(echo $file | cut -d '_' -f3)$(echo $file | cut -d '_' -f4)
+            fi
+            if [ "$filedt" -ge "$(echo ${stt[ii]} | tr -d '_')00" ] && [ "$filedt" -lt "$(echo ${edt[ii]} | tr -d '_')00" ]; then 
+                echo $filepath >> $configdir/$tfile
+                echo $(basename $filepath)
+            fi
+            if [ "$filedt" -ge "$(echo ${edt[ii]} | tr -d '_')00" ]; then
+                break
+            fi
+        done
+
     done
 
     mv $configdir/$tfile $configdir/$doppfile
@@ -243,25 +302,39 @@ else
         echo MP=$(echo $mpname2 | tr '[:lower:]' '[:upper:]')
         echo
 
-        inputfile2=input_${mpname2}_${stt}_${edt}.txt
-        configfile2=config_${mpname2}_${stt}_${edt}.txt
+        inputfile2=input_${mpname2}_${stt[0]}_${edt[${#edt[@]}-1]}.txt
+        configfile2=config_${mpname2}_${stt[0]}_${edt[${#edt[@]}-1]}.txt
 
         inputfiles2+=($configdir/$inputfile2)
         configfiles2+=($configdir/$configfile2)
         allmps+=($mpname2)
 
-        tfile=input_${stt}_${edt}.txt
+        tfile=input_${stt[0]}_${edt[${#edt[@]}-1]}.txt
 
-        for filepath in $(ls $simdir/* | sort); do
-            file=$(basename $filepath)
-            filedt=$(echo $file | cut -d '_' -f4 | tr -d '-')$(echo $file | cut -d '_' -f5 | cut -d '.' -f1 | tr -d ':')
-            if [ "$filedt" -ge "$(echo $stt | tr -d '_')00" ] && [ "$filedt" -lt "$(echo $edt | tr -d '_')00" ]; then 
-                echo $filepath >> $configdir/$tfile
-                echo $(basename $filepath)
-            fi
-            if [ "$filedt" -ge "$(echo $edt | tr -d '_')00" ]; then
-                break
-            fi
+        for ((ii=0;ii<${#starg[@]};ii++)); do
+            
+            first=${stt[ii]:0:4}-${stt[ii]:4:2}-${stt[ii]:6:2}_${stt[ii]:9:2}
+            last=${edt[ii]:0:4}-${edt[ii]:4:2}-${edt[ii]:6:2}_${edt[ii]:9:2}
+            fmt=%Y-%m-%d_%H
+            len=13
+            dats=$(echo ${first:0:$len})
+            while [[ $(echo $first | tr -d '-' | tr -d '_' | tr -d ':') -lt $(echo $last | tr -d '-' | tr -d '_' | tr -d ':') ]]; do
+                first=$(date -j -v +1H -f $fmt $first +$fmt)
+                dats=$dats"\|"${first:0:$len}
+            done
+
+            for filepath in $(ls $simdir/* | grep "$dats" | sort); do
+                file=$(basename $filepath)
+                filedt=$(echo $file | cut -d '_' -f4 | tr -d '-')$(echo $file | cut -d '_' -f5 | cut -d '.' -f1 | tr -d ':')
+                if [ "$filedt" -ge "$(echo ${stt[ii]} | tr -d '_')00" ] && [ "$filedt" -lt "$(echo ${edt[ii]} | tr -d '_')00" ]; then 
+                    echo $filepath >> $configdir/$tfile
+                    echo $(basename $filepath)
+                fi
+                if [ "$filedt" -ge "$(echo ${edt[ii]} | tr -d '_')00" ]; then
+                    break
+                fi
+            done
+
         done
 
         mv $configdir/$tfile $configdir/$inputfile2
@@ -270,7 +343,7 @@ else
 
 fi
 
-outfigdir=outputfig/${fold}_temp${tempsrc}_${station}_${stt}_${edt}
+outfigdir=outputfig/${fold}_temp${tempsrc}_${station}_${stt[0]}_${edt[${#edt[@]}-1]}
 outrrdir=$(cd $raddir/../../ && pwd)/radar_rainrates/$station
 mkdir -p $outfigdir $outrrdir
 
@@ -291,8 +364,8 @@ if [ -z $simdir ]; then
         sed -i '' "s/.*mphys ==.*/mphys == $mpname == # Type of microphysics used in model: 'obs' OR '<scheme>' if type = 'wrf'/g" $configdir/$configfile
     fi
     sed -i '' "s/.*ptype ==.*/ptype == '$ptype' == # Output figure file extenstion (i.e. png, jpg, mp4, ...)/g" $configdir/$configfile
-    sed -i '' "s/.*sdatetime ==.*/sdatetime == '$(echo $stt | tr '_' '-')' == # Start time of analysis of interest/g" $configdir/$configfile
-    sed -i '' "s/.*edatetime ==.*/edatetime == '$(echo $edt | tr '_' '-')' == # End time of analysis of interest/g" $configdir/$configfile
+    sed -i '' "s/.*sdatetime ==.*/sdatetime == '$(echo ${stt[0]} | tr '_' '-')' == # Start time of analysis of interest/g" $configdir/$configfile
+    sed -i '' "s/.*edatetime ==.*/edatetime == '$(echo ${edt[${#edt[@]}-1]} | tr '_' '-')' == # End time of analysis of interest/g" $configdir/$configfile
     sed -i '' "s%.*rfiles ==.*%rfiles == '$configdir/$inputfile' == # Path to list of radar files to read in%g" $configdir/$configfile
     if [[ "$wrft_on" == "True" ]]; then
         sed -i '' "s%.*wfiles ==.*%wfiles == '$configdir/$tempfile' == # Path to list of WRF temperature files to read in%g" $configdir/$configfile
@@ -337,8 +410,8 @@ else
     sed -i '' "s/^type ==.*/type == obs == # Type of input data: 'obs' OR 'wrf' (obs + simulated)/g" $configdir/$configfile
     sed -i '' "s/.*mphys ==.*/mphys == obs == # Type of microphysics used in model: 'obs' OR '<scheme>' if type = 'wrf'/g" $configdir/$configfile
     sed -i '' "s/.*ptype ==.*/ptype == '$ptype' == # Output figure file extenstion (i.e. png, jpg, mp4, ...)/g" $configdir/$configfile
-    sed -i '' "s/.*sdatetime ==.*/sdatetime == '$(echo $stt | tr '_' '-')' == # Start time of analysis of interest/g" $configdir/$configfile
-    sed -i '' "s/.*edatetime ==.*/edatetime == '$(echo $edt | tr '_' '-')' == # End time of analysis of interest/g" $configdir/$configfile
+    sed -i '' "s/.*sdatetime ==.*/sdatetime == '$(echo ${stt[0]} | tr '_' '-')' == # Start time of analysis of interest/g" $configdir/$configfile
+    sed -i '' "s/.*edatetime ==.*/edatetime == '$(echo ${edt[${#edt[@]}-1]} | tr '_' '-')' == # End time of analysis of interest/g" $configdir/$configfile
     sed -i '' "s%.*rfiles ==.*%rfiles == '$configdir/$inputfile' == # Path to list of radar files to read in%g" $configdir/$configfile
     if [[ "$wrft_on" == "True" ]]; then
         sed -i '' "s%.*wfiles ==.*%wfiles == '$configdir/$tempfile' == # Path to list of WRF temperature files to read in%g" $configdir/$configfile
@@ -368,8 +441,8 @@ else
         sed -i '' "s/^type ==.*/type == wrf == # Type of input data: 'obs' OR 'wrf' (obs + simulated)/g" ${configfiles2[ii]}
         sed -i '' "s/.*mphys ==.*/mphys == $(echo ${allmps[ii]}) == # Type of microphysics used in model: 'obs' OR '<scheme>' if type = 'wrf'/g" ${configfiles2[ii]}
         sed -i '' "s/.*ptype ==.*/ptype == '$ptype' == # Output figure file extenstion (i.e. png, jpg, mp4, ...)/g" ${configfiles2[ii]}
-        sed -i '' "s/.*sdatetime ==.*/sdatetime == '$(echo $stt | tr '_' '-')' == # Start time of analysis of interest/g" ${configfiles2[ii]}
-        sed -i '' "s/.*edatetime ==.*/edatetime == '$(echo $edt | tr '_' '-')' == # End time of analysis of interest/g" ${configfiles2[ii]}
+        sed -i '' "s/.*sdatetime ==.*/sdatetime == '$(echo ${stt[0]} | tr '_' '-')' == # Start time of analysis of interest/g" ${configfiles2[ii]}
+        sed -i '' "s/.*edatetime ==.*/edatetime == '$(echo ${edt[${#edt[@]}-1]} | tr '_' '-')' == # End time of analysis of interest/g" ${configfiles2[ii]}
         sed -i '' "s%.*rfiles ==.*%rfiles == '${inputfiles2[ii]}' == # Path to list of radar files to read in%g" ${configfiles2[ii]}
         sed -i '' "s%.*wfiles ==.*%wfiles == '${inputfiles2[ii]}' == # Path to list of WRF temperature files to read in%g" ${configfiles2[ii]}
         if [[ "$dd_on" == "True" ]]; then
